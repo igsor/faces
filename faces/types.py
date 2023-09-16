09191fb0b5a61ceb46ed94b786e60c26bd5db2dc
Matthias Baumgartner <dev@igsor.net>
2023-09-16 16:23:03 +0200

    feat: create image from numpy array

--- faces/types.py
+++ faces/types.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Optional, Tuple
 
 import torch
+from numpy.typing import NDArray
 from PIL import Image as PILImage
 
 from faces.utils import preprocess
@@ -61,6 +62,24 @@ class Image:
         return cls(
             preprocess(
                 PILImage.open(path),
+                target_size=target_size,
+                rotate=rotate,
+            )
+        )
+
+    @classmethod
+    def from_array(
+        cls,
+        buffer: NDArray,
+        target_size: int = 1000,
+        rotate: Optional[int] = None,
+    ) -> Image:
+        """Load and preprocess an image from a numpy array *buffer*.
+        See `faces.utils.preprocess` for the *target_size* and *rotate* parameters.
+        """
+        return cls(
+            preprocess(
+                PILImage.fromarray(buffer),
                 target_size=target_size,
                 rotate=rotate,
             )
--- test/test_types.py
+++ test/test_types.py
@@ -1,10 +1,24 @@
 import unittest
+from pathlib import Path
+
+import numpy as np
+from PIL import Image as PILImage
 
 from faces import BoundingBox, Image
 
 
 class TestImage(unittest.TestCase):
-    pass
+    def test_from_array(self) -> None:
+        image = Image.from_array(
+            np.array(
+                PILImage.open(Path(__file__).parent / "data" / "images" / "cactus.jpg")
+            )
+        )
+        self.assertEqual(image.image.size, (1000, 664))
+
+    def test_open(self) -> None:
+        image = Image.open(Path(__file__).parent / "data" / "images" / "cactus.jpg")
+        self.assertEqual(image.image.size, (1000, 664))
 
 
 class TestBoundingBox(unittest.TestCase):
